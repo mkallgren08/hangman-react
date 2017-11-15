06d6c7ef5a828c95e811fa1ec74bead54266d53a
Michael <mkallgren08@gmail.com>
2017-11-14 23:36:58 -0500

    Starting to work through the logic of hangman

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -9,31 +9,39 @@ import { Input, TextArea, FormBtn } from "../../components/Form";
 
 class Articles extends Component {
   state = {
-    searchterm : "",
-    numberofrecords : 5,
-    startyear : "",
-    endyear : "",
+    searchterm: "",
+    numberofrecords: 5,
+    startyear: "",
+    endyear: "",
     articles: [],
-    savedarticles: []
+    savedarticles: [],
+    wordtoguess: "",
+    blanks: ""
   };
+
+  // Selection of valid characters for the game
+  characSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z',  " "]
 
   // Initial load of saved articles
   componentDidMount() {
     this.loadSavedArticles("");
+    this.loadWordToGuess();
   }
 
   // code to get saved articles
   loadSavedArticles = () => {
     API.getArticles()
       .then(
-        res => {
-          this.setState({ savedarticles: res.data })
-        })
+      res => {
+        this.setState({ savedarticles: res.data })
+      })
       // console.log(res.data.response.docs);
       .catch(err => console.log(err));
   };
 
-  
+
   // handle form input
   handleInputChange = event => {
     // Destructure the name and value properties off of event.target
@@ -44,20 +52,32 @@ class Articles extends Component {
     });
   };
 
+  //loads a random entry from a selected category
+  //for testing purposes now, using a static entry
+  loadWordToGuess = () => {
+    let words = /*["flamingo", "ocelot", "pistol shrimp"]*/ ["pistol shrimp"]
+    let word = words[Math.floor(Math.random() * words.length)]
+    let wordsplit = word.split("")
+    console.log(wordsplit)
+    this.setState({ wordtoguess: word })
+  }
+
+
   // search NYT for articles
   handleFormSubmit = event => {
     // When the form is submitted, prevent its default behavior, get recipes update the recipes state
     event.preventDefault();
     if (this.state.searchterm) {
       APINYT.search(this.state.searchterm,
-                    this.state.numberofrecords,
-                    this.state.startyear,
-                    this.state.endyear
-                    )
-        .then(res => { this.setState({ articles: res.data.response.docs });
+        this.state.numberofrecords,
+        this.state.startyear,
+        this.state.endyear
+      )
+        .then(res => {
+          this.setState({ articles: res.data.response.docs });
         })
         .catch(err => console.log(err));
-    } 
+    }
   };
 
   //save an article
@@ -65,7 +85,7 @@ class Articles extends Component {
     // When the form is submitted, prevent its default behavior, get recipes update the recipes state
     API.saveArticle(data)
       .then(res => this.loadSavedArticles())
-        // console.log(res.data.response.docs);
+      // console.log(res.data.response.docs);
       .catch(err => console.log(err));
   };
 
@@ -84,8 +104,40 @@ class Articles extends Component {
         <Row>
           <Col size="md-12">
             <Jumbotron>
-              <h1>New York Times Search</h1>
+              <h1>Hangman</h1>
             </Jumbotron>
+            <div className="panel panel-primary">
+              <div className="panel-heading">
+                <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Word to Guess</strong></h3>
+              </div>
+              <div className="panel-body" id="well-section">
+                <List>
+                  <ListItem>
+                    <h3>{this.state.wordtoguess}</h3>
+                  </ListItem>
+                </List>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <div className="panel panel-primary">
+              <div className="panel-heading">
+                <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Word to Guess</strong></h3>
+              </div>
+              <div className="panel-body" id="well-section">
+                <List>
+                  <ListItem>
+                    <h3>{this.state.wordtoguess}</h3>
+                  </ListItem>
+                </List>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
             <div className="panel panel-primary">
               <div className="panel-heading changeme">
                 <h3 className="panel-title"><strong><i className="fa  fa-list-alt"></i>  Search Parameters</strong></h3>
@@ -94,60 +146,62 @@ class Articles extends Component {
                 <form>
                   <label htmlFor="searchterm">Search Term:</label>
                   <Input name="searchterm"
-                         value={this.state.searchterm}
-                         onChange={this.handleInputChange}
-                         placeholder="Search Term" />
+                    value={this.state.searchterm}
+                    onChange={this.handleInputChange}
+                    placeholder="Search Term" />
                   <label htmlFor="numberofrecords">Number of Records:</label>
-                  <select name="numberofrecords" 
-                          value={this.state.numberofrecords}
-                          onChange={this.handleInputChange}
-                          className="form-control" 
-                          id="num-records-select">
+                  <select name="numberofrecords"
+                    value={this.state.numberofrecords}
+                    onChange={this.handleInputChange}
+                    className="form-control"
+                    id="num-records-select">
                     <option value="1">1</option>
-						      	<option value="5" >5</option>
+                    <option value="5" >5</option>
                     <option value="10">10</option>
                   </select>
                   <label htmlFor="startyear">Start Year (optional - must be 4 digit year):</label>
                   <Input name="startyear"
-                         value={this.state.startyear}
-                         onChange={this.handleInputChange} 
-                         placeholder="Start Year" />
+                    value={this.state.startyear}
+                    onChange={this.handleInputChange}
+                    placeholder="Start Year" />
                   <label htmlFor="endyear">End Year (optional - must be 4 digit year):</label>
-                  <Input name="endyear" 
-                         value={this.state.endyear}
-                         onChange={this.handleInputChange}
-                         placeholder="End Year" />
-                  <button type="submit" 
-                          className="btn btn-default" 
-                          onClick={this.handleFormSubmit}
-                          id="run-search"><i className="fa fa-search"></i> Search</button>
+                  <Input name="endyear"
+                    value={this.state.endyear}
+                    onChange={this.handleInputChange}
+                    placeholder="End Year" />
+                  <button type="submit"
+                    className="btn btn-default"
+                    onClick={this.handleFormSubmit}
+                    id="run-search"><i className="fa fa-search"></i> Search</button>
                   {"     "}
                   <button type="button" className="btn btn-default" id="clear-all"><i className="fa fa-trash"></i> Clear Results</button>
                 </form>
-             </div>
+              </div>
             </div>
           </Col>
         </Row>
         {/* Results */}
         <Row>
-            <Col size="md-12">
-             <div className="panel panel-primary">
-               <div className="panel-heading">
-                  <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Top Articles</strong></h3>
+          <Col size="md-12">
+            <div className="panel panel-primary">
+              <div className="panel-heading">
+                <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Top Articles</strong></h3>
               </div>
               <div className="panel-body" id="well-section">
                 {this.state.articles.length ? (
                   <List>
-                    {this.state.articles.slice(0, this.state.numberofrecords).map((article,index) => (
+                    {this.state.articles.slice(0, this.state.numberofrecords).map((article, index) => (
                       <ListItem key={article.pub_date}>
                         <h3>{article.headline.main}</h3>
                         <a href={article.web_url}>{article.web_url} </a>
                         <h3>{article.pub_date}</h3>
                         <button type="submit"
                           className="btn btn-default"
-                          onClick={() => this.handleArticleSave({title: article.headline.main, 
-                                                                url: article.web_url,
-                                                                date: article.pub_date})}
+                          onClick={() => this.handleArticleSave({
+                            title: article.headline.main,
+                            url: article.web_url,
+                            date: article.pub_date
+                          })}
                           id="saveArticle"><i className="fa fa-search"></i> Save</button>
                       </ListItem>
                     ))}
@@ -156,13 +210,13 @@ class Articles extends Component {
                     <h3>No Results to Display</h3>
                   )}
               </div>
-             </div>
+            </div>
           </Col>
         </Row>
         {/* Saved Articles */}
         <Row>
           <Col size="md-12">
-            
+
             <div className="panel panel-primary">
               <div className="panel-heading">
                 <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Saved Articles</strong></h3>
@@ -170,7 +224,7 @@ class Articles extends Component {
               <div className="panel-body" id="well-section">
                 {this.state.savedarticles.length ? (
                   <List>
-                    {this.state.savedarticles.map((article, index) =>  (
+                    {this.state.savedarticles.map((article, index) => (
                       <ListItem key={article._id}>
                         <h3>{article.title}</h3>
                         <a href={article.url}>{article.url} </a>
